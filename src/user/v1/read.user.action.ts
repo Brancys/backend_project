2161101b1d25cf5f7d42416c4cbf4ce0eee4c097
Brancys <brancysb@uninorte.edu.co>
2024-11-02 10:20:00 -0500

    hashing passwords

--- src/user/v1/read.user.action.ts
+++ src/user/v1/read.user.action.ts
@@ -1,4 +1,5 @@
 import { UserModel, UserType } from "./user.model";
+import argon2 from 'argon2';
 
 // DECLARE ACTION FUNCTION
 async function readUserAction(): Promise<UserType[]> {
@@ -11,6 +12,14 @@ async function readUserbyIDAction(id: string): Promise<UserType | null> {
   return result;
 }
 
+async function verifyPassword(storedPassword: string, inputPassword: string): Promise<boolean> {
+  try {
+      return await argon2.verify(storedPassword, inputPassword);
+  } catch (error) {
+      console.error("Error verifying password:", error);
+      return false;
+  }
+} 
 
 // EXPORT ACTION FUNCTION
-export default {readUserAction, readUserbyIDAction};
+export default {readUserAction, readUserbyIDAction, verifyPassword};
--- src/user/v1/user.model.ts
+++ src/user/v1/user.model.ts
@@ -1,10 +1,13 @@
-import { model, Schema } from "mongoose";
+import { model, Schema, CallbackError } from "mongoose";
+import argon2 from 'argon2';    
 
 // DECLARE MODEL TYPE
 type UserType = {
     _id: string;
     name: string;
     cedula: string;
+    email: string;
+    password: string;
 };
 
 // DECLARE MONGOOSE SCHEMA
@@ -15,10 +18,34 @@ const UserSchema = new Schema<UserType>({
     },
     cedula: {
         type: String
-    }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/\S+@\S+\.\S+/, 'El email no es válido'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, 'La contraseña debe tener al menos 8 caracteres'],
+        maxlength: [15, 'La contraseña no puede tener más de 15 caracteres'],
+    },
 },{
     timestamps: true,
     versionKey: false,
+});
+
+// Middleware para hashear la contraseña antes de guardarla
+UserSchema.pre('save', async function (next) {
+    const user = this as UserType;
+
+    try {
+        user.password = await argon2.hash(user.password);
+        next();
+    } catch (error) {
+        next(error as CallbackError);
+    }
 });
 
 // DECLARE MONGO MODEL
