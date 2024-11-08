3cd7de2ba488a09cb0708e36b3fddad0d7a1d4f9
Brancys <brancysb@uninorte.edu.co>
2024-11-08 14:32:45 -0500

    reestructuring

--- src/book/v1/book.types.ts
+++
@@ -1,4 +0,0 @@
-import { BookType } from "./book.model";
-
-export type CreateBookType = Omit<BookType, "_id">
-export type UpdateBookType = Omit<Partial<BookType>, "_id">
--- src/user/v1/delete.user.action.ts
+++
@@ -1,7 +0,0 @@
-import { UserModel, UserType } from "./user.model";
-
-export async function softDeleteUserAction(userId: string) {
-    return await UserModel.findByIdAndUpdate(userId, { state: false }, { new: true });
-}
-
-export default softDeleteUserAction;
